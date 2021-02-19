c174ee610fa63d0ccb40247ee0001693ea5d513e
Mart Nael <martnael84@gmail.com>
2021-02-19 12:04:16 +0200

    Poorise 5 done Poorise 7 one thing to handle still

--- src/main/java/com/adaur/Crawler/RealEstateCrawlerApplication.java
+++ src/main/java/com/adaur/Crawler/RealEstateCrawlerApplication.java
@@ -1,12 +1,15 @@
 package com.adaur.Crawler;
 
+import com.adaur.Crawler.services.TtpServices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class RealEstateCrawlerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(RealEstateCrawlerApplication.class, args);
 	}
 
--- src/main/java/com/adaur/Crawler/services/TtpServices.java
+++ src/main/java/com/adaur/Crawler/services/TtpServices.java
@@ -1,6 +1,5 @@
 package com.adaur.Crawler.services;
 
-import com.sun.xml.bind.v2.runtime.output.SAXOutput;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,57 +7,95 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 @Service
 public class TtpServices {
 
-    public static void main(String[] args) throws IOException {
-        pooriseCrawler();
-    }
+    private String developer = "TTP AS";
+    private String[] ttpActiveProjectLinks = {"https://poorise.uusmaa.ee/houses/poorise-5/",
+                                                "https://poorise.uusmaa.ee/houses/poorise-7/",
+                                                "https://poorise.uusmaa.ee/houses/poorise-3/",
+                                                "https://www.jarvetornid.ee/torn-2-korterid/",
+                                                "https://www.jarvetornid.ee/aripinnad/"
+                                                };
+    String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
 
+    /**
+     * Main crawler engine for TTP project Poorise
+     * @throws IOException
+     * Date Last Modified: 19-02-2021
+     * Author: Mart Nael
+     */
 
     public static void pooriseCrawler() throws IOException {
-        String[] pooriseLinks = {"https://poorise.uusmaa.ee/houses/poorise-5/", "https://poorise.uusmaa.ee/houses/poorise-7/", "https://poorise.uusmaa.ee/houses/poorise-3/"};
         String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
-        Document pagePoorise = Jsoup.connect(pooriseLinks[0]).userAgent(userAgent).get();
+        String[] ttpActiveProjectLinks = {"https://poorise.uusmaa.ee/houses/poorise-5/",
+                "https://poorise.uusmaa.ee/houses/poorise-7/",
+                "https://poorise.uusmaa.ee/houses/poorise-3/",
+                "https://www.jarvetornid.ee/torn-2-korterid/",
+                "https://www.jarvetornid.ee/aripinnad/"
+        };
+
+        String unitUrl = "";
+        String unitNumber = "";
+        int unitFloorNumber = 0;
+        int unitRoomsNumber = 0;
+        double unitSize = 0;
+        double unitBalconySize = 0;
+        BigDecimal unitPrice = BigDecimal.ZERO;
+        String unitStatus = "";
+
+
+        Document pagePoorise = Jsoup.connect(ttpActiveProjectLinks[1]).userAgent(userAgent).get();
         Elements poorisePlan = pagePoorise.select("div.content.active.tab_content").select("table");
         for (Element trElement : poorisePlan.select("tr")) {
-            if (trElement.hasClass("div.flat_row.has_link")) {
-
-            }
-            
-                String unitUrl = trElement.attr("data-link");
-                String unitNumber = trElement.select("td.flat_nr").text();
-                String unitFloor = trElement.select("td.floor").text();
-                String unitRooms = trElement.select("td.rooms").text();
-                String unitSize = trElement.select("td.size").text();
-                String unitBalcony = trElement.select("td.balcony").text();
-                String unitPrice;
-                String unitStatus;
-                if (trElement.hasClass("price.demoapartment")) {
-                    unitPrice = null;
-                    unitStatus = trElement.select("td.price.demoapartment").text();
-                } else if (trElement.hasClass("price.sold")) {
-                    unitPrice = null;
-                    unitStatus = trElement.select("td.price.sold").text();
-                } else if ((trElement.select("td.price").text()).equals("Broneeritud")) {
-                    unitPrice = null;
-                    unitStatus = trElement.select("td.price").text();
+            if (trElement.hasClass(" flat_row  has_link")) {
+                unitUrl = trElement.attr("data-link");
+                unitNumber = trElement.select("td.flat_nr").text();
+                unitFloorNumber = Integer.parseInt(trElement.select("td.floor").text());
+                unitRoomsNumber = Integer.parseInt(trElement.select("td.rooms").text());
+                unitSize = Double.parseDouble(trElement.select("td.size").text());
+                unitBalconySize = Double.parseDouble(trElement.select("td.balcony").text());
+                if ((trElement.select("td.price  ").text()).startsWith("N")) {
+                    unitPrice = BigDecimal.ZERO;
+                    unitStatus = "Naidiskorter";
                 } else {
-                    unitPrice = trElement.select("td.price").text();
+                    unitPrice = new BigDecimal(trElement.select("td.price  ").text());
                     unitStatus = "Vaba";
                 }
+            } else if (trElement.hasClass(" flat_row  ")) {
+                unitUrl = trElement.attr("data-link");
+                unitNumber = trElement.select("td.flat_nr").text();
+                unitFloorNumber = Integer.parseInt(trElement.select("td.floor").text());
+                unitRoomsNumber = Integer.parseInt(trElement.select("td.rooms").text());
+                unitSize = Double.parseDouble(trElement.select("td.size").text());
+                unitBalconySize = Double.parseDouble(trElement.select("td.balcony").text());
+                if (trElement.hasClass("price reserved ")) {
+                    unitPrice= BigDecimal.ZERO;
+                    unitStatus = "Broneeritud";
+                } else {
+                    unitPrice= BigDecimal.ZERO;
+                    unitStatus = "Muudud";
+                }
+
+            }
                 System.out.println(unitUrl);
                 System.out.println(unitNumber);
-                System.out.println(unitFloor);
-                System.out.println(unitRooms);
+                System.out.println(unitFloorNumber);
+                System.out.println(unitRoomsNumber);
                 System.out.println(unitSize);
-                System.out.println(unitBalcony);
+                System.out.println(unitBalconySize);
                 System.out.println(unitPrice);
                 System.out.println(unitStatus);
                 System.out.println("<---------->");
 
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        pooriseCrawler();
+    }
+
 }
 
